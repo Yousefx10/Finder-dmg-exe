dcd19dc0c000916504f52ac35bfc90f3efaa95f9
Yousef Khaled <yousef_espetan@yahoo.com>
2025-10-06 16:19:03 +0300

    Add project files.

---
+++ Finder_dmg_exe/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Finder_dmg_exe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
