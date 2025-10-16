20936ea760a50476d612d27c1ed6eca8e5388f18
Yousef Khaled <yousef_espetan@yahoo.com>
2025-10-16 07:08:24 +0300

    Define SideBar Custom Control

---
+++ Finder_dmg_exe/View/UserControls/SideBar.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing.IndexedProperties;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finder_dmg_exe.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SideBar.xaml
+    /// </summary>
+    public partial class SideBar : UserControl
+    {
+        public SideBar()
+        {
+            InitializeComponent();
+        }
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                "Text",
+                typeof(string),
+                typeof(SideBar),
+                new PropertyMetadata("Element"));
+
+
+    }
+}
